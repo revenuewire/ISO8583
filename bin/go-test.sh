f4a93e431f7147a4e58c3b3fedb7051907b30059
Scott Wang <swang@revenuewire.com>
2019-03-15 10:50:40 -0700

    try this

--- bin/go-test.sh
+++ bin/go-test.sh
@@ -2,5 +2,7 @@
 
 docker-compose down
 docker-compose run --rm unittest
+docker-compose run --rm unittest php ./vendor/bin/php-coveralls -v -x ./build/logs/clover.xml
+
 docker-compose down
 docker container prune -f
