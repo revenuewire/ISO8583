a76babea7eed2dda0768632b8a1bf543d4378865
Scott Wang <swang@revenuewire.com>
2019-03-15 10:59:46 -0700

    try

--- bin/go-test.sh
+++ bin/go-test.sh
@@ -2,7 +2,6 @@
 
 docker-compose down
 docker-compose run --rm unittest
-docker-compose run --rm unittest php ./vendor/bin/php-coveralls -v -x ./build/logs/clover.xml
 
 docker-compose down
 docker container prune -f
